a21f38db588b32b671830b02df2982347c385519
CaueFer <cauefernandescaetano@gmail.com>
2023-06-13 15:22:35 -0300

    integrate DB com php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'ziemmotors';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+?>
