5cc829ffcde711fafaee83ee3848a6ee53ada3b0
CaueFer <cauefernandescaetano@gmail.com>
2023-06-30 19:19:20 -0300

    searchbar other pages

--- Estoque/estoque.php
+++ Estoque/estoque.php
@@ -262,8 +262,6 @@ if (isset($_SESSION['email']) == true) {
     </footer>
 </body>
 
-
-<script src="../geral.js?v=1.45"></script>
 <script src="estoque.js?v=1.45"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js?v=1.45"
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
