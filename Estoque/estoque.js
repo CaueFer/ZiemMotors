a798dcd5fa5b0c1f7a9653e1259cbfe90d2852e8
CaueFer <cauefernandescaetano@gmail.com>
2023-07-06 16:46:58 -0300

    bigpage fix nav

--- Estoque/estoque.js
+++ Estoque/estoque.js
@@ -315,8 +315,8 @@ function createBigPage(e) {
 
                         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                             <li><a href="../Estoque/estoque.php" class="nav-link px-3 link-body-emphasis navTitle">Estoque</a></li>
-                            <li><a href="#" class="nav-link px-3 link-body-emphasis navTitle">Contato</a></li>
-                            <li><a href="#" class="nav-link px-3 link-body-emphasis navTitle">Sobre</a></li>
+                            <li><a href="../Contato/contato.php" class="nav-link px-3 link-body-emphasis navTitle">Contato</a></li>
+                            <li><a href="../Sobrenos/sobrenos.php" class="nav-link px-3 link-body-emphasis navTitle">Sobre</a></li>
                         </ul>
 
                         <div class="dropdown text-end">
